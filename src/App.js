7203518afa98e3256c8d36c6da65ff7ede2652cb
roshan_agrawal <f20190191@pilani.bits-pilani.ac.in>
2024-02-11 06:14:00 +0530

    open ai api embedded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ImageGenerator from './Components/ImageGenerator/ImageGenerator';
+
+function App() {
+  return (
+    <div>
+
+      <ImageGenerator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from 'react'
+import './ImageGenerator.css'
+import default_image from '../Assets/iterate_ai_logo.svg'
+
+const ImageGenerator = () => {
+
+    const [image_url, setImage_url]= useState("/")
+    let inputRef= useRef(null);
+
+    const imageGenerator = async () =>{
+        if(inputRef.current.value===""){
+            return 0;
+        }
+        const response = await fetch(
+            "https://api.openai.com/",
+            {
+                method:"POST",
+                headers:{
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer ",
+                    "User-Agent":"Chrome",
+                },
+                body:JSON.stringify({
+                    prompt:'${inputRef.current.value}',
+                    n:1, 
+                    size: "512x512"
+                }),
+            }
+        );
+        let data = await response.json();
+        console.log(data);
+    }
+    return (
+        <div className="ai-image-generator">
+        <div className="header">Product Idea <span>generator</span></div>
+        <div className="img-loading">
+            <div className="image"><img src={image_url==="/"?default_image:image_url}/></div>
+        </div>
+        <div className="search-box">
+            <input type="text" ref={inputRef} className='search-input' placeholder='Write your Product idea'/>
+            <div className="generate-btn" onClick={()=> {imageGenerator()}}>Generate</div>
+        </div>
+        </div>
+    )
+}
+
+export default ImageGenerator
